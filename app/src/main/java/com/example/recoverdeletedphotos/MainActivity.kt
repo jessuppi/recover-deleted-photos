6c2ae4e68bb3d777fa9fb7c2942b78de00bdcf70
Jesse Nickles <jesse@littlebizzy.com>
2025-10-03 22:16:14 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/recoverdeletedphotos/MainActivity.kt
+++ app/src/main/java/com/example/recoverdeletedphotos/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.recoverdeletedphotos
 
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = TextView(this)
-        tv.text = "Recover Deleted Photos"
+
+        val tv = TextView(this).apply {
+            text = getString(R.string.app_name)
+            gravity = Gravity.CENTER
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        }
+
         setContentView(tv)
     }
 }
