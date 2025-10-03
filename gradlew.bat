246bc83afcafdacff780a879357773c62be3701b
Jesse Nickles <jesse@littlebizzy.com>
2025-10-03 22:43:48 +0700

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM ##########################################################################
-REM
-REM  Gradle startup script for Windows
-REM
-REM ##########################################################################
-set DIR=%~dp0
-"%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
