c42610cada19e0aa19a19524727babeaac5bb057
Jesse Nickles <jesse@littlebizzy.com>
2025-10-06 14:41:50 +0700

    Create ScanFragment.kt

---
+++ app/src/main/java/top/thinapps/recoverdeletedphotos/ui/ScanFragment.kt
@@ -0,0 +1,57 @@
+package top.thinapps.recoverdeletedphotos.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import top.thinapps.recoverdeletedphotos.R
+import top.thinapps.recoverdeletedphotos.databinding.FragmentScanBinding
+import top.thinapps.recoverdeletedphotos.scan.MediaScanner
+
+class ScanFragment : Fragment() {
+    private var _vb: FragmentScanBinding? = null
+    private val vb get() = _vb!!
+    private var job: Job? = null
+
+    override fun onCreateView(inflater: LayoutInflater, c: ViewGroup?, s: Bundle?): View {
+        _vb = FragmentScanBinding.inflate(inflater, c, false)
+        return vb.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        vb.cancelButton.setOnClickListener { cancel() }
+        start()
+    }
+
+    private fun start() {
+        job = viewLifecycleOwner.lifecycleScope.launch {
+            vb.progress.setProgressCompat(0, false)
+            val items = MediaScanner(requireContext()).scan { pct ->
+                vb.progress.setProgressCompat(pct, true)
+                vb.percent.text = getString(R.string.percent_format, pct)
+            }
+            // small delay for UX smoothness
+            delay(250)
+            findNavController().navigate(
+                R.id.action_scan_to_results,
+                ResultsFragment.args(items)
+            )
+        }
+    }
+
+    private fun cancel() {
+        job?.cancel()
+        requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    override fun onDestroyView() {
+        _vb = null
+        super.onDestroyView()
+    }
+}
