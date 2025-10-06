ff052dac09afffe812195aa077f6b9bb96ac112e
Jesse Nickles <jesse@littlebizzy.com>
2025-10-06 14:39:01 +0700

    Update MainActivity.kt

--- app/src/main/java/top/thinapps/recoverdeletedphotos/MainActivity.kt
+++ app/src/main/java/top/thinapps/recoverdeletedphotos/MainActivity.kt
@@ -1,12 +1,25 @@
 package top.thinapps.recoverdeletedphotos
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import top.thinapps.recoverdeletedphotos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var vb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // ui-only shell for 0.5.0: toolbar + static chips + empty state
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+
+        setSupportActionBar(vb.toolbar)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHost.navController
+        vb.toolbar.setupWithNavController(navController)
     }
 }
