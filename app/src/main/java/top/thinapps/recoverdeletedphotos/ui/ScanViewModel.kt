b7f341880236db5e054f2bbb3c34615d376d56a2
Jesse Nickles <jesse@littlebizzy.com>
2025-10-18 02:57:23 +0700

    Update ScanViewModel.kt

--- app/src/main/java/top/thinapps/recoverdeletedphotos/ui/ScanViewModel.kt
+++ app/src/main/java/top/thinapps/recoverdeletedphotos/ui/ScanViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import top.thinapps.recoverdeletedphotos.model.MediaItem
 
 class ScanViewModel : ViewModel() {
-    // holds scan results across fragments
+
+    // holds scan results across fragments and survives config changes
     var results: List<MediaItem> = emptyList()
 }
